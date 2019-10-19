1ded427ff42adb7b7c218af9c54acc619682d058
Girmay <ink423103@yahoo.com>
2019-10-19 20:48:35 +0200

    Updates

---
+++ precache-manifest.f65c85a74ceb5d75e8e63413fa6d017b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab2a6f305aabcd1c5952bda42677719",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f2c315d457ca0d0e9c7e",
+    "url": "/my-app/static/css/main.b679c532.chunk.css"
+  },
+  {
+    "revision": "5885525d649db42590db",
+    "url": "/my-app/static/js/2.f7f465ee.chunk.js"
+  },
+  {
+    "revision": "f2c315d457ca0d0e9c7e",
+    "url": "/my-app/static/js/main.dea6e2f3.chunk.js"
+  },
+  {
+    "revision": "247eec36cf986955c323",
+    "url": "/my-app/static/js/runtime-main.fe96fb05.js"
+  }
+]);
