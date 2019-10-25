084e98267e586bf0afbabc29afbfd5b16d81bdef
Girmay <ink423103@yahoo.com>
2019-10-25 23:30:30 +0200

    component for todoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  constructor(props) {
+		super(props)
+
+		this.state = {
+      title: 'Edit',
+      inputType: "checkbox"			
+		}   
+    };
   getStyle = () => {
     return {
       background:'#00FFFF',  
@@ -11,19 +19,36 @@ export class TodoItem extends Component {
       textDecoration: this.props.todo.completed ? 'line-through' : 'none'
     }
   }
+  // Delete Todo
+  updateTodo = (id) => { 
+    if(this.state.title === 'Edit'){
+      const inputValue = document.querySelector('input.inputCheckbox').innerHTML;
+      this.setState({title: 'Update'});
+      this.setState({inputType: 'text'});
+      document.querySelector('input.inputCheckbox').value= inputValue;
+    }      
+    else{
+      this.setState({title: 'Edit'});
+      this.setState({inputType: 'checkbox'});
+    } 
+    console.log(this.state.title);   
+};
 
   render() {
-    const { id, description } = this.props.todo;
+    const { id, description , deadline} = this.props.todo;
+    const {title} = this.state;   
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete.bind(this, id) } style={checkboxStyle}/> {' '}
-          { description }
+          <input type= {this.state.inputType} onChange={this.props.markComplete.bind(this, id) } 
+          style={checkboxStyle} className = 'inputCheckbox'/> {' '}
+          { description } | {deadline}
           <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>Delete</button>
+          <button onClick={this.updateTodo} style={editBtnStyle}>{''} {title}</button> 
         </p>
       </div>
     )
-  }
+  } 
 }
 
 // PropTypes
@@ -40,7 +65,16 @@ const btnStyle = {
   padding: '10px 19px',
   borderRadius: '50%',
   cursor: 'pointer',
-  float: 'right'
+  float: 'right'  
+}
+const editBtnStyle={
+  background: '#ffA500',
+  color: '#fff',
+  border: 'none',
+  padding: '10px 19px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'  
 }
 const checkboxStyle = {
   boxShadow: '0 0 0 5px orange'
